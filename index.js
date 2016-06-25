e5109b0e8cf5ddcf3ee681323e5d8f46f331e4ae
Mark Sundt <marksundt@gmail.com>
2016-06-25 15:54:14 -0500

    Index Page

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + "/clearenthpp.html"));
+});
+
+
+app.post('/charge', function(req, res) {
+    console.log('Here is the response:');
+    console.log(res.body);
+
+});
+
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
